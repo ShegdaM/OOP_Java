1595135299a8143f04e68ef84e557df2141f611e
ShegdaM <36561271+ShegdaM@users.noreply.github.com>
2018-06-03 20:10:19 +0300

    Add files via upload

---
+++ Laba 12/Main12.java
@@ -0,0 +1,53 @@
+package ua.lpnuai.oop.shegda12;
+
+import java.util.*;
+import java.lang.String;
+import java.lang.Character;
+
+
+public class Checker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a Password: ");
+        String password = input.next();
+        if (isValid(password)) {
+            System.out.println("Valid Password");
+        } else {
+            System.out.println("Invalid Password");
+        }
+    }
+
+    public static boolean isValid(String password) {
+        if (password.length() < 8) {
+            return false;
+        } else {
+            char c;
+            int count = 1;
+            for (int i = 0; i < password.length() - 1; i++) {
+                c = password.charAt(i);
+                if (!Character.isLetterOrDigit(c)) {
+                    return false;
+                } else if (Character.isDigit(c)) {
+                    count++;
+                    if (count < 2)   {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public  boolean is_digit(String str)
+    {
+        try
+        {
+            double d = Double.parseDouble(str);
+        }
+        catch(NumberFormatException nfe)
+        {
+            return false;
+        }
+        return true;
+    }
+}
